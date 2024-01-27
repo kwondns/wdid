e639df0b72a0f2f53cc0007912e6a97607f1e3b9
Hanjun Kwon <kwondns@gmail.com>
2024-01-28 02:12:56 +0900

    🎉 Init: Project 필요 라이브러리 설치
    - Recoil: 로컬 상태관리
    - React-Query: 원격 상태관리
    - supabase-js: Supabase 연결
    - TailwindCSS: 스타일링
    - DaisyUI: Tailwind Component Library

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,74 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
+    'airbnb',
+    'airbnb-typescript',
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+    'plugin:tailwindcss/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  overrides: [],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react-refresh', 'react', '@typescript-eslint'],
+  settings: {
+    tailwindcss: {
+      config: './tailwind.config.ts',
+    },
+  },
   rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+    'import/extensions': 0,
+    // * react
+    'react/jsx-props-no-spreading': [
+      1,
+      {
+        custom: 'ignore',
+      },
+    ],
+    'react/react-in-jsx-scope': 0,
+    "react-hooks/exhaustive-deps": [
+      "warn",
+      {
+        "additionalHooks": "useRecoilCallback"
+      }
+    ],
+    'import/prefer-default-export': 1,
+    'import/no-extraneous-dependencies': 0,
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external'],
+        'newlines-between': 'always',
+      },
+    ],
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      {
+        allowConstantExport: true,
+      },
     ],
+    // * typescript
+    '@typescript-eslint/no-shadow': ['warn'],
+    // * react-query
+    '@tanstack/query/exhaustive-deps': 'error',
+    '@tanstack/query/no-rest-destructuring': 'warn',
+    '@tanstack/query/stable-query-client': 'error',
   },
-}
+};
