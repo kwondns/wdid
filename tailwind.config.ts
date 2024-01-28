ee655e1f229c9eba23476ffa56ee4ddb8c6cf0f6
Hanjun Kwon <kwondns@gmail.com>
2024-01-28 11:34:53 +0900

    🔨 Chore: Tailwind Safelist 추가
    - Todo Card z-index 동적 부여를 위해 z-[0] ~ z-[10] 추가
    - /z-\d+/ 형태로 정규식으로 하려 했으나 [ ] 대괄호가 들어가면 인식을 못하는 문제
    - 임시 방편으로 수동으로 z-0 부터 z-10까지 추가
    - Todo Card 스타일 하기 위해 `-left-*` 으로 left 스타일도 추

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,4 +7,18 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
+  safelist: [
+    'z-[0]',
+    'z-[1]',
+    'z-[2]',
+    'z-[3]',
+    'z-[4]',
+    'z-[5]',
+    'z-[6]',
+    'z-[7]',
+    'z-[8]',
+    'z-[9]',
+    'z-[10]',
+    { pattern: /-left-*/ },
+  ],
 };
