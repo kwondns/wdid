f0644f1f50649ae5cc6b72bedd16b1ce9cc9e1b8
Hanjun Kwon <kwondns@gmail.com>
2024-01-29 11:18:24 +0900

    🚀 Feat: React-Query 연결
    - React-Router-dom의 loader에서 queryClient를 사용하기에 router에서 설정
    - queryClient의 suspense등 추가 옵션 추후 설정 예정
    - 이전 Todo들을 볼 수 있는 history와 같은 페이지도 라우트 추가 예정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,22 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Index from 'pages';
+import Index, { indexLoader } from 'pages';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const router = createBrowserRouter([{ path: '/', element: <Index /> }]);
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000 * 10,
+    },
+  },
+});
+const router = createBrowserRouter([{ path: '/', element: <Index />, loader: indexLoader(queryClient) }]);
 
 export default function Router() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
