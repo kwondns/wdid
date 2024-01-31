50a7027ece8fd6962446f20254b69f3e9eb84293
Hanjun Kwon <kwondns@gmail.com>
2024-01-31 13:53:57 +0900

    🚀 Feat: Loader 수정
    - Todo만 존재하던 loader를 수정하여 HowMany, WhatDid에 대한 데이터 수집 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,33 @@
 import { QueryClient } from '@tanstack/react-query';
+import { useRecoilValue } from 'recoil';
 
-import { useTodos } from '@/hooks';
-import { TodoTemplate } from '@/templates';
-import { TodoType } from '@/types';
+import { useHowMany, useTodos, useWhatDid } from '@/hooks';
+import { TodoTemplate, HowManyTemplate, WhatDidTemplate } from '@/templates';
+import { TodoType, HowManyType, WhatDidType } from '@/types';
+import { ActivityStore } from '@/stores';
 
+type LoaderType = {
+  Todo: TodoType.TodoViewType[];
+  HowMany: HowManyType.HowManyType[];
+  WhatDid: WhatDidType.WhatDidType[];
+};
 export default function Index() {
+  const activityDate = useRecoilValue(ActivityStore.ActivityDateAtom);
   return (
-    <div className="flex h-screen flex-1 items-center justify-center">
+    <div className="grid h-screen grid-cols-[55%_45%] grid-rows-[auto_30%_70%]">
+      <div className="col-start-1 col-end-2 row-start-2 row-end-4 flex h-full flex-col gap-y-2 p-2">
+        <HowManyTemplate />
+        <div className="flex w-full flex-col border-opacity-50">
+          <div className="divider">{activityDate}</div>
+        </div>
+        <WhatDidTemplate />
+      </div>
       <TodoTemplate />
     </div>
   );
 }
 
-export const indexLoader = (queryClient: QueryClient) => async (): Promise<TodoType.TodoViewType[]> => {
+export const indexLoader = (queryClient: QueryClient) => async (): Promise<LoaderType> => {
   async function enableMocking() {
     if (process.env.NODE_ENV !== 'development') {
       return;
@@ -22,7 +37,13 @@ export const indexLoader = (queryClient: QueryClient) => async (): Promise<TodoT
     return worker.start();
   }
   return enableMocking().then(async () => {
-    const query = useTodos.useTodosAll();
-    return queryClient.getQueryData(query.queryKey) ?? (await queryClient.fetchQuery(query));
+    const queryTodo = useTodos.useTodosAll();
+    const queryHowMany = useHowMany.useHowManyAll();
+    const queryWhatDid = useWhatDid.useWhatDid(new Date().toLocaleDateString());
+    return {
+      Todo: queryClient.getQueryData(queryTodo.queryKey) ?? (await queryClient.fetchQuery(queryTodo)),
+      HowMany: queryClient.getQueryData(queryHowMany.queryKey) ?? (await queryClient.fetchQuery(queryHowMany)),
+      WhatDid: queryClient.getQueryData(queryWhatDid.queryKey) ?? (await queryClient.fetchQuery(queryWhatDid)),
+    };
   });
 };
