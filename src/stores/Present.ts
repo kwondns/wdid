175683e1a66206f391d52fe90f845f6fe18dbea9
Hanjun Kwon <kwondns@gmail.com>
2024-02-01 12:02:00 +0900

    🚀 Feat: Present Store 생성
    - 현재 시간을 Atom으로 관리
    - Effect를 사용하여 1분마다 최신 상태를 유지
    - 현재 시간 업데이트 시 diffTime 경과시간도 자동으로 변경
    - StartTime, EndTime, DiffTime 전부 Recoil로 관리
    - DiffTime은 바로 정제된 문자열로 반환하도록 Selector 생

---
+++ src/stores/Present.ts
@@ -0,0 +1,63 @@
+import { atom, AtomEffect, selector } from 'recoil';
+
+import { DateLib } from '@/libs';
+
+export const MarkdownAtom = atom({
+  key: 'markdownAtom',
+  default: '',
+});
+
+const CurrentTimeEffect =
+  (): AtomEffect<Date> =>
+  ({ setSelf }) => {
+    const now = new Date();
+    const timeUntilNextMinute = (60 - now.getSeconds()) * 1000 - now.getMilliseconds();
+
+    // 정각에 맞춰 첫 업데이트를 실행
+    const timeoutId = setTimeout(() => {
+      setSelf(new Date());
+
+      // 그 후 1분마다 업데이트
+      const intervalId = setInterval(() => {
+        setSelf(new Date());
+      }, 60 * 1000);
+
+      return () => clearInterval(intervalId);
+    }, timeUntilNextMinute);
+
+    return () => clearTimeout(timeoutId);
+  };
+export const CurrentTimeAtom = atom({
+  key: 'currentTimeAtom',
+  default: new Date(),
+  effects: [CurrentTimeEffect()],
+});
+export const StartTimeAtom = atom<Date | null>({
+  key: 'startTimeAtom',
+  default: null,
+});
+
+export const EndTimeAtom = atom<Date | null>({
+  key: 'endTimeAtom',
+  default: null,
+});
+
+export const DiffTimeAtom = atom<string | null>({
+  key: 'diffTimeAtom',
+  default: null,
+});
+
+export const DiffTimeSelector = selector({
+  key: 'diffTimeSelector',
+  get: ({ get }) => {
+    const startTime = get(StartTimeAtom);
+    const endTime = get(EndTimeAtom);
+    const currentTime = get(CurrentTimeAtom);
+    if (startTime) {
+      if (endTime) {
+        return DateLib.fromDate(startTime, endTime);
+      }
+      return DateLib.fromDate(startTime, currentTime);
+    }
+  },
+});
