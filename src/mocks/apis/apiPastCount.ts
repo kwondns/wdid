52db96bc4b70a40030b0baaf0174525102716a5b
Hanjun Kwon <kwondns@gmail.com>
2024-02-03 14:14:25 +0900

    🚚 File: Mock 제거
    - 배포를 위해 mock 제거

--- src/mocks/apis/apiPastCount.ts
+++
@@ -1,4 +0,0 @@
-import { FetchLib } from '@/libs';
-import { PastCountType } from '@/types';
-
-export const getPastCountAll = async () => FetchLib.fetchGet<PastCountType.PastCountType[]>('pastcount');
--- src/mocks/handler.ts
+++
@@ -1,7 +0,0 @@
-import FutureHandler from './futureHandler';
-import FutureBoxHandler from './futureBoxHandler';
-import PastCountHandler from './pastCountHandler';
-import PastHandler from './pastHandler';
-import presentHandler from './presentHandler';
-
-export const handlers = [...FutureBoxHandler, ...FutureHandler, ...PastCountHandler, ...PastHandler, ...presentHandler];
