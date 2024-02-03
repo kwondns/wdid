7ef44017b850fac65eb308c686984ca302361d38
Hanjun Kwon <kwondns@gmail.com>
2024-02-03 10:28:27 +0900

    🔨 Chore: supabase type gen
    - script gen-type supabase type generate
    - schema timeline 지정
    - 미지정시 불필요한 public 스키마까지 포함되어 지정
    - Table 생성과 같은 타입은 public이 포함되어 에러발생
    - Table 생성 및 업데이트는 이 프로젝트에서 필요없어 제거
    - supabase init 후 생성 파일

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,5 +3,6 @@ import * as FutureBoxType from './FutureBox';
 import * as PastCountType from './PastCount';
 import * as PastType from './Past';
 import * as PresentType from './Present';
+import * as SupabaseType from './supabase';
 
-export { FutureType, FutureBoxType, PastCountType, PastType, PresentType };
+export { FutureType, FutureBoxType, PastCountType, PastType, PresentType, SupabaseType };
