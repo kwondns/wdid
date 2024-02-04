656d695b4736d913584c8385a0a07d03a6eb72fa
Hanjun Kwon <kwondns@gmail.com>
2024-02-04 09:04:21 +0900

    🚑️ Fix: Safari Date객체 생성 오류
    - Safari 기반의 브라우저에서는 toLocaleDateString()으로 생성된 문자열의 Date생성불가
    - new Date(new Date().toLocaleDateString()) <- Invalid Date
    - 이 문제를 해결하기 위해 API를 요청하기 전에 처리하는 과정 추가
    
    - date.slice(0, -1)
    - 2024. 2. 4
    - split('. ')
    - ['2024', '2', '4']
    - join('/')
    - '2024/2/4'
    - YYYY/mm/dd 형태의 문자열은 인식가능하여 버그 해결

--- src/apis/apiPast.ts
+++ src/apis/apiPast.ts
@@ -2,11 +2,12 @@ import { Supabase } from '@/libs';
 import { PastType } from '@/types';
 
 export const getPast = async (date: string) => {
+  const dateFormat = date.slice(0, -1).split('. ').join('/');
   const { data, error } = await Supabase.supabase
     .from('past')
     .select('*')
-    .gte('created_at', new Date(date).toISOString())
-    .lte('created_at', new Date(new Date(date).getTime() + 60 * 1000 * 60 * 24 - 1).toISOString())
+    .gte('created_at', new Date(dateFormat).toISOString())
+    .lte('created_at', new Date(new Date(dateFormat).getTime() + 60 * 1000 * 60 * 24 - 1).toISOString())
     .order('created_at', { ascending: true });
   Supabase.errorCheck(error);
   return data;
