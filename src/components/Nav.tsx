8f6b00418b7a81741d67affb8b99d3655b9c9088
Hanjun Kwon <kwondns@gmail.com>
2024-02-04 15:42:17 +0900

    💄 Design: Nav 디자인 변경
    - <Link>의 영역을 최대로 하여 <div> 영역 어디든 링크 효과를 얻도록 수정
    - Active시 배경색 효과를 <div>에서 나타나도록 변경
    - Active시 Icon의 사이즈를 중앙에서 125% 커지도록 변경

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,28 +8,37 @@ export default function Nav() {
   const layout = useRecoilValue(LayoutStore.MobileSelector);
   return (
     <nav className={`${layout === 'mobile' ? 'btm-nav z-[10000]' : 'h-full w-auto'}`}>
-      <div className="tooltip tooltip-bottom" data-tip="Past">
-        <Link className={`btn btn-ghost ${pathname === '/past' ? 'rounded-2xl bg-slate-500/60' : ''}`} to="/past">
+      <div
+        className={`tooltip tooltip-bottom ${pathname === '/past' ? 'rounded-2xl bg-slate-500/60' : ''}`}
+        data-tip="Past"
+      >
+        <Link className="btn btn-ghost size-full" to="/past">
           <img
-            className={`size-[50px] p-2 ${pathname === '/past' ? 'scale-110' : ''}`}
+            className={`size-[50px] origin-center p-2 ${pathname === '/past' ? 'scale-125' : ''}`}
             src="/assets/past.svg"
             alt="past"
           />
         </Link>
       </div>
-      <div className="tooltip tooltip-bottom" data-tip="Present">
-        <Link className={`btn btn-ghost ${pathname === '/present' ? 'rounded-2xl bg-slate-500/60' : ''}`} to="/present">
+      <div
+        className={`tooltip tooltip-bottom ${pathname === '/present' ? 'rounded-2xl bg-slate-500/60' : ''}`}
+        data-tip="Present"
+      >
+        <Link className="btn btn-ghost size-full" to="/present">
           <img
-            className={`size-[50px] p-2 ${pathname === '/present' ? 'scale-110' : ''}`}
+            className={`size-[50px] origin-center p-2 ${pathname === '/present' ? 'scale-125' : ''}`}
             src="/assets/present.svg"
             alt="present"
           />
         </Link>
       </div>
-      <div className="tooltip tooltip-bottom" data-tip="Future">
-        <Link className={`btn btn-ghost ${pathname === '/future' ? 'rounded-2xl bg-slate-500/60' : ''}`} to="/future">
+      <div
+        className={`tooltip tooltip-bottom ${pathname === '/future' ? 'rounded-2xl bg-slate-500/60' : ''}`}
+        data-tip="Future"
+      >
+        <Link className="btn btn-ghost size-full" to="/future">
           <img
-            className={`size-[50px] p-2 ${pathname === '/future' ? 'scale-110' : ''}`}
+            className={`size-[50px] origin-center p-2 ${pathname === '/future' ? 'scale-125' : ''}`}
             src="/assets/future.svg"
             alt="future"
           />
