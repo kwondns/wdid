bac432b3c93847c5879161d40e778f330dcbbf08
Hanjun Kwon <kwondns@gmail.com>
2024-02-04 15:11:29 +0900

    🚀 Feat: Layout Width 감지
    - Layout Store의 MobileAtom, MobileSelector 생성
    - MobileAtom -> 현재 화면의 너비값을 저장
    - MobileEffect -> Atom 이펙트로 리사이즈 이벤트를 추적하여 현재 너비로 갱신
    - MobileSelector -> Atom에 저장된 값에 따라 mobile, tablet, desktop 반환
    - 768px미만 mobile
    - 1024미만 tablet
    - 그 이상 desktop
    
    - Mobile 변환 시 Header의 Nav가 하단으로 이동
    - 따라서 <main>영역의 크기 감소

--- src/templates/Layout.tsx
+++ src/templates/Layout.tsx
@@ -1,14 +1,21 @@
+import { useRecoilValue } from 'recoil';
+
 import { Header, CarouselContainer } from '@/components';
+import { LayoutStore } from '@/stores';
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 export default function Layout(props: LayoutProps) {
   const { children } = props;
+  const isMobile = useRecoilValue(LayoutStore.MobileSelector);
+  let mainHeight: string;
+  if (isMobile === 'mobile') mainHeight = 'h-[calc(100vh-128px)]';
+  else mainHeight = 'h-[calc(100vh-64px)]';
   return (
     <div className="flex max-h-screen flex-col overflow-hidden">
       <Header />
-      <main className="h-[calc(100vh-64px)]">
+      <main className={mainHeight}>
         <CarouselContainer>{children}</CarouselContainer>
       </main>
     </div>
