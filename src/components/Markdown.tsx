48a44e501fd0798e428df148ae49f118da5c9ed1
Hanjun Kwon <kwondns@gmail.com>
2024-02-08 23:01:57 +0900

    🚀 Feat: MD Image Lazy Loading
    - rehypePlugin을 생성하여 에디터에 추가
    - 이미지를 lazy loading을 하게 하기위하여 플러그인 작성
    - <img src='...' loading='lazy' /> 적용 완

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,5 +1,7 @@
 import MDEditor from '@uiw/react-md-editor';
 
+import { MarkdownLib } from '@/libs';
+
 type MarkdownProps = {
   source: string;
 };
@@ -9,6 +11,14 @@ export default function Markdown(props: MarkdownProps) {
     <MDEditor.Markdown
       className="!bg-base-200 [&_img]:mx-auto [&_img]:!flex [&_img]:max-h-[500px] [&_ul]:list-disc"
       source={source}
+      rehypeRewrite={(node, _, parent) => {
+        if ('tagName' in node && node.tagName && parent && 'tagName' in parent && parent.tagName) {
+          if (node.tagName === 'a' && /^h([1-6])/.test(parent.tagName)) {
+            parent.children = parent.children.slice(1);
+          }
+        }
+      }}
+      rehypePlugins={[MarkdownLib.imgLazyLoading]}
     />
   );
 }
