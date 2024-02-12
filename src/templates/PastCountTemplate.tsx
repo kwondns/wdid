eeddf177dac728cce8192e740c2e6bf141ce67bd
Hanjun Kwon <kwondns@gmail.com>
2024-02-12 20:47:45 +0900

    💄 Design: Chart 생성 후 디자인 수정
    - 캘린더를 기본 지정된 사이즈만큼 차지
    - 그 외 길이 차트를 최대 길이로 하는 반응형 사이즈
    - md (768px) 이상 일 경우 flex-row
    - 그 이하는 가독성 측면에서 flex-col 로 변경

--- src/templates/PastCountTemplate.tsx
+++ src/templates/PastCountTemplate.tsx
@@ -3,14 +3,15 @@ import { useQuery } from '@tanstack/react-query';
 
 import { pastLoader } from '@/pages';
 import { usePastCount } from '@/hooks';
-import { ActivityCalendar } from '@/components';
+import { ActivityCalendar, ActivityChart } from '@/components';
 
 export default function PastCountTemplate() {
   const { PastCount: initialData } = useLoaderData() as Awaited<ReturnType<ReturnType<typeof pastLoader>>>;
   const { data } = useQuery({ ...usePastCount.usePastCountAll(), initialData });
   return (
-    <div className="self-center p-1">
+    <div className="flex w-full flex-col items-center justify-center gap-2 self-center p-1 md:flex-row md:p-6">
       <ActivityCalendar activities={data} />
+      <ActivityChart activities={data} />
     </div>
   );
 }
