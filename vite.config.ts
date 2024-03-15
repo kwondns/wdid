ab7d98ebdf819d11284292a92e3f3f9ec4223e29
kwondns <155633601+kwondns@users.noreply.github.com>
2024-03-16 08:03:32 +0900

    🚑️ Fix: Chunk 분할 수정
    - Chunk 분할 제거 후 벤더 분리만 유지
    - 매니페스트 잘못된 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,28 +5,4 @@ import { visualizer } from 'rollup-plugin-visualizer';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), splitVendorChunkPlugin(), visualizer()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id: string) {
-          if (id.toUpperCase().includes('PAST')) return '@past';
-          if (id.toUpperCase().includes('PRESENT')) return '@present';
-          if (id.toUpperCase().includes('FUTURE')) return '@future';
-          if (
-            id.includes('markdown') ||
-            id.includes('remark') ||
-            id.includes('rehype') ||
-            id.includes('md') ||
-            id.includes('editor') ||
-            id.includes('refractor')
-          )
-            return '@md';
-          if (id.includes('react-router-dom') || id.includes('react-router') || id.includes('remix')) {
-            return '@react-router';
-          }
-          if (id.includes('recoil')) return '@recoil';
-        },
-      },
-    },
-  },
 });
