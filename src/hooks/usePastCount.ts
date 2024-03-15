1f10e9fa4778204d4682db3e6e0638faaab4d53b
kwondns <155633601+kwondns@users.noreply.github.com>
2024-03-15 16:26:33 +0900

    🚚 File: 미사용 파일 제거
    - API 마이그레이션으로 인한 불필요한 파일 및 패턴 변화로 제거

--- src/hooks/usePastCount.ts
+++
@@ -1,6 +0,0 @@
-import { apiPastCount } from '@/apis';
-
-export const usePastCountAll = () => ({
-  queryKey: ['past', 'count'],
-  queryFn: apiPastCount.getPastCountAll,
-});
--- src/libs/index.ts
+++
@@ -1,6 +0,0 @@
-import * as FetchLib from './fetchLibs';
-import * as DateLib from './dateLibs';
-import * as MarkdownLib from './markdownLibs';
-import * as Supabase from './supabase';
-
-export { FetchLib, DateLib, MarkdownLib, Supabase };
